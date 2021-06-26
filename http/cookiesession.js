7ae948428ec063cbb4cfb9b8f808e4e9128004b5
Oracle Public Cloud User <opc@myinstance.public.krnet.oraclevcn.com>
2021-06-26 01:47:32 +0000

    revise

--- http/cookiesession.js
+++ http/cookiesession.js
@@ -29,7 +29,7 @@ http.createServer(async (req,res) => {
     res.end(`Welcome ${cookies.name}`);
   } else {
     try {
-      const data = await fs.readFile('./cookie2.html');
+      const data = await fs.readFile('./cookie.html');
       res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8'});
       res.end(data);
   }catch (err) {
